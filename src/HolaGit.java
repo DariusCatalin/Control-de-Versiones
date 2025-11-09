8e92c9f9444a2dcdad34b2ae356eaa9207699b30
DariusCatakin <djdarius995@alumnes.ilerna.com>
2025-11-09 18:37:04 +0100

    Mensaje desde la rama Darius añadido en el archivo HolaGit.java

--- src/HolaGit.java
+++ src/HolaGit.java
@@ -1,5 +1,6 @@
 public class HolaGit {
-public static void main (String[] args) {
-System.out.println("Hola Github!");
-}
+    public static void main(String[] args) {
+        System.out.println("Hola Github!");
+        System.out.println("Hola desde la rama de Darius!");
+    }
 }
