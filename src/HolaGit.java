145d3bd2fc7d362087e6e9138dbd6b2d7b2ba505
adonis25042 <adonis25042@alumnes.ilerna.com>
2025-11-10 22:27:38 +0100

    FIN: Mi ciclo de versionado ha terminado con exito.

--- src/HolaGit.java
+++ src/HolaGit.java
@@ -1,5 +1,7 @@
 public class HolaGit {
-public static void main (String[] args) {
-System.out.println("Hola Github!");
-}
+    public static void main(String[] args) {
+        System.out.println("Hola Github!");
+        // AÑADE ESTA LÍNEA ABAJO:
+        System.out.println("¡Saludos del colaborador Unai! Ya he creado mi rama."); 
+    }
 }
